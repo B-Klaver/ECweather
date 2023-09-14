20267bde18985a7554a5fb80afa9c5844128f790
B-Klaver <braedenklaver@gmail.com>
2023-09-14 11:48:07 -0700

    major update: - removed the need for the getECurls function. - changed how the data downloads and the data downloading checks

--- R/getECdata.R
+++ R/getECdata.R
@@ -33,7 +33,6 @@
 #' @references https://climate.weather.gc.ca/historical_data/search_historic_data_e.html
 #' https://collaboration.cmc.ec.gc.ca/cmc/climate/Get_More_Data_Plus_de_donnees/
 #' https://collaboration.cmc.ec.gc.ca/cmc/climate/Get_More_Data_Plus_de_donnees/Station_Inventory_ID_Disclaimer_Metadata_EN.txt
-#' @seealso  This function wraps the function ECweather::getECurls()
 #' @examples
 #' #An example that includes saving the data to the local working directory
 #' getECdata(stations = c(52), year_start = 2022, year_end = 2023,
@@ -47,6 +46,7 @@
 getECdata <- function(stations, year_start, year_end, timeframe = c("hourly", "daily", "monthly"),
                       download = FALSE, folder = NULL, verbose = TRUE, delete = TRUE) {
 
+  #set in lower case
   timeframe <- tolower(timeframe)
 
   #quickly check the timeframe parameter
@@ -66,201 +66,155 @@ getECdata <- function(stations, year_start, year_end, timeframe = c("hourly", "d
     }
 
     if (!dir.exists(folder)) {
-      message(paste("Creating the following folder: ", folder))
-
-      create_folder <- try(dir.create(folder, showWarnings = FALSE))
-
-      if (isFALSE(create_folder)) {
-        stop(paste0("Failed to create folder '", folder,
-             "'. Check path and permissions."))
-      }
+      stop("Folder path doesn't exist.")
     }
   }
 
 
   #GENERATE URLS FOR EACH STATION TO PULL DATA
+
   urls <- stations %>%
     purrr::map(~ {
-              getECurls(.,
-                      year_start,
-                      year_end,
-                      timeframe = timeframe)
-              })
 
+      #create a vector of years to build URLs
+      years <- year_start:year_end
 
+      #Generate vectors to supply to URL
+      if (timeframe == "hourly") {
+
+        years <-  rep(years, each = 12)
+        months <- rep(1:12, times = length(years))
+        ids <- rep(., length(years) * 12)
+        time_index <- 1
+
+      } else if (timeframe == "daily") {
+
+        months <- 1
+        ids <- rep(., length(years))
+        time_index <- 2
+
+      } else if (timeframe == "monthly") {
+
+        years <- year_start
+        months <- 1
+        ids <- .
+        time_index <- 3
+
+      }
+
+      #Fill the URLs
+      urls <- paste0("http://climate.weather.gc.ca/climate_data/bulk_data_e.html?&format=csv&stationID=", .,
+                     "&Year=", years,
+                     "&Month=", months,
+                     "&Day=14",
+                     "&timeframe=", time_index,
+                     "&submit= Download+Data")
+
+      #return a list of URLs for each station ID
+      return(list(urls = urls,
+                  ids = ids))
+
+    })
 
   ## Extract the data from the URLs generation
   url_paths <- unlist(lapply(urls, function(url_list) url_list$urls))
   sites <- unlist(lapply(urls, function(url_list) url_list$ids))
-  years <- unlist(lapply(urls, function(url_list) url_list$years))
-  months <- unlist(lapply(urls, function(url_list) url_list$months))
-  nfiles <- length(url_paths)
-
-  ## filenames to use to save the data
-  if (download == TRUE) {
-    fnames <- paste(sites, years, months, "data.csv", sep = "_")
-    fnames <- file.path(folder, fnames)
-  }
-
-
+  n_paths <- length(url_paths)
 
 
   ## set up a progress bar if being verbose
   if (isTRUE(verbose)) {
-    pb <- utils::txtProgressBar(min = 0,
-                                max = nfiles,
+    progress <- utils::txtProgressBar(min = 0,
+                                max = n_paths,
                                 style = 3)
 
-    on.exit(close(pb))
+    on.exit(close(progress))
   }
 
   #setup a list to fill
-  #out <- vector(mode = "list", length = nfiles)
-  out <- list("data" = NULL, "fails" = NULL)
+  out <- list("data" = NULL,
+              "fails" = NULL)
 
   #iterate the download over the files
-  for (i in seq_len(nfiles)) {
-
-    if (download == TRUE) {
-      #get current file
-      curfile <- fnames[i]
-
-      #DOWNLOADING FILE
-      utils::download.file(url_paths[i], destfile = curfile, quiet = TRUE)
-
-      ## Try reading the file
-      ecdata <- try(data.table::fread(curfile, encoding = "Latin-1", stringsAsFactors = FALSE,
-                                      fill = TRUE, showProgress = FALSE),
-                    silent = TRUE)
+  for (i in seq_len(n_paths)) {
 
 
+    #try to read the data
+    ecdata <- try(data.table::fread(url_paths[i],
+                                    encoding = "Latin-1",
+                                    stringsAsFactors = FALSE,
+                                    fill = TRUE,
+                                    showProgress = FALSE),
+                  silent = TRUE)
+
+      #If the URL doesn't hold data it will give a strange first column
       if (stringr::str_detect(colnames(ecdata)[1], "DOCTYPE")) {
 
-        ecdata <- readLines(curfile, warn = FALSE) # read all lines in file
-
-        encoding <- stringi::stri_enc_detect(ecdata)$Encoding
-
-        if (is.null(encoding)) {
-
-          out$fails <- append(out$fails, url_paths[i])
-
-
-          if (delete) {
-
-            file.remove(curfile) # remove file if a problem & deleting
-
-          }
-
-          if (isTRUE(verbose)) { # Update the progress bar
-
-            utils::setTxtProgressBar(pb, value = i)
-
-          }
-
-
-          next
-
-        }
-
-        ecdata <- iconv(ecdata, encoding, to = "UTF-8") #convert to UTF-8
-        writeLines(ecdata, curfile)          # write the data back to the file
-
-        ## try to read the file again, if still an error, bail out
-        ecdata <- try(data.table::fread(curfile, encoding = "UTF-8", stringsAsFactors = FALSE,
-                                        fill = TRUE, showProgress = FALSE),
-                      silent = TRUE)
-
-        if (inherits(ecdata, "try-error") | str_detect(colnames(ecdata)[1], "DOCTYPE")) { # yes, still!, handle read problem
-
-          out$fails <- append(out$fails, url_paths[i])    # record failed URL...
-
-
-          if (delete) {
-
-            file.remove(curfile) # remove file if a problem & deleting
-
-          }
-
-
-          if (isTRUE(verbose)) {
-
-            utils::setTxtProgressBar(pb, value = i) # update progress bar...
-
-          }
-
-          next                  # bail out of current iteration
-
-        }
-
-      }
-
-    }
-
-    if(download == FALSE) {
-
-      ecdata <- try(data.table::fread(url_paths[i], encoding = "Latin-1", stringsAsFactors = FALSE,
-                                      fill = TRUE, showProgress = FALSE),
-                    silent = TRUE)
-
-      if (stringr::str_detect(colnames(ecdata)[1], "DOCTYPE")) {
-
+        #read the lines
         ecdata <- readLines(url_paths[i], warn = FALSE) # read all lines in file
 
-        encoding <- stringi::stri_enc_detect(ecdata)$Encoding
+        #check the encoding
+        ec_encoding <- stringi::stri_enc_detect(ecdata)$Encoding
 
+        #If encoding is null it tells us the data failed to download
         if (is.null(encoding)) {
+
+          #label as a failed url
+          out$fails <- append(out$fails, url_paths[i])
+
+          # Update the progress bar
+          if (isTRUE(verbose)) {
+
+            utils::setTxtProgressBar(progress, value = i)
+
+          }
+
+
+          #next iteration
+          next
+
+        }
+
+        ## try to read the file again using the encoding found above
+        ecdata <- try(data.table::fread(url_paths[i],
+                                        encoding = ec_encoding,
+                                        stringsAsFactors = FALSE,
+                                        fill = TRUE,
+                                        showProgress = FALSE),
+                      silent = TRUE)
+
+        #If still no luck then throw into the failed pile and continue
+        if (inherits(ecdata, "try-error") | str_detect(colnames(ecdata)[1], "DOCTYPE")) {
 
           out$fails <- append(out$fails, url_paths[i])
 
 
-          if (isTRUE(verbose)) { # Update the progress bar
+          if (isTRUE(verbose)) {
 
-            utils::setTxtProgressBar(pb, value = i)
+            utils::setTxtProgressBar(progress, value = i)
 
           }
-
 
           next
 
         }
 
-        ## try to read the file again, if still an error, bail out
-        ecdata <- try(data.table::fread(url_paths[i], encoding = "UTF-8", stringsAsFactors = FALSE,
-                                        fill = TRUE, showProgress = FALSE),
-                      silent = TRUE)
-
-        if (inherits(ecdata, "try-error") | str_detect(colnames(ecdata)[1], "DOCTYPE")) { # yes, still!, handle read problem
-
-          out$fails <- append(out$fails, url_paths[i])    # record failed URL...
-
-
-          if (isTRUE(verbose)) {
-
-            utils::setTxtProgressBar(pb, value = i) # update progress bar...
-
-          }
-
-          next                  # bail out of current iteration
-
-        }
-
       }
 
-    }
 
-    ## If we read the file successfully, add on the station id
-    ecdata <- cbind.data.frame(station_id = rep(sites[i],
-                                                nrow(ecdata)),
+    ## If we read the file successfully, add on the station id and do clean up
+    ecdata <- cbind.data.frame(station_id = rep(sites[i], nrow(ecdata)),
                                ecdata) %>%
       janitor::clean_names() %>%
       dplyr::mutate_all(as.character)
 
-    #add onto the list
+    #add the data onto the list
     out$data[[as.character(sites[i])]] <- dplyr::bind_rows(out$data[[as.character(sites[i])]], ecdata)
 
-    if (isTRUE(verbose)) { # Update the progress bar
+    # Update the progress bar
+    if (isTRUE(verbose)) {
 
-      utils::setTxtProgressBar(pb, value = i)
+      utils::setTxtProgressBar(progress, value = i)
 
     }
 
@@ -268,15 +222,18 @@ getECdata <- function(stations, year_start, year_end, timeframe = c("hourly", "d
 
   #return the failed downloads
   if (length(out$fails) > 0) {
-    warning("The following URLs failed to download:\n", paste(out$fails, collapse = "\n"))
+    warning("The following URLs failed to download, please check your parameters:\n", paste(out$fails, collapse = "\n"))
   }
-
 
   #return the list of dataframes
   out <- dplyr::bind_rows(out$data)
 
   return(out)
 
+  #download data if selected
+  if(download = TRUE){
+    fwrite(out, file.path(folder, paste0("ECdata_", timeframe, "_", year_start, "_", year_end, ".csv")))
+  }
 
 }
 
